77ddf4fef2dbcc54b2fea891957596f0513e3278
willianjairo2003 <willianqccanazas@gmail.com>
2025-08-23 12:04:51 -0500

    MI PRIMER COMMIT

---
+++ proyectoavance/src/main/java/com/example/proyectoavance/ProyectoavanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoavance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoavanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoavanceApplication.class, args);
+	}
+
+}
---
+++ proyectoavance/src/test/java/com/example/proyectoavance/ProyectoavanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoavance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoavanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
