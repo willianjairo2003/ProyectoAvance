b3a0b92f580acbe74cf3e4a3af9fbe879f967034
willianjairo2003 <willianqccanazas@gmail.com>
2025-09-16 23:20:28 -0500

    nuevo commit

--- proyectoavance/src/main/java/com/example/proyectoavance/ControlPaginas.java
+++ proyectoavance/src/main/java/com/example/proyectoavance/ControlPaginas.java
@@ -1,8 +1,12 @@
 package com.example.proyectoavance;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControlPaginas {
@@ -44,13 +48,30 @@ public class ControlPaginas {
     	return "carritocompras";
     }
     
-
-
-    // Procesar login (sin validar)
-    @PostMapping("/procesarLogin")
-    public String procesarLogin() {
-        return "principal"; 
+    @GetMapping("/panelAdmin")
+    public String panelAdmin(Model model) {
+        model.addAttribute("mensaje", "Bienvenido al Panel de Administración");
+        return "panelAdmin"; // busca panelAdmin.html en templates
     }
+    
+
+
+    @PostMapping("/procesarLogin")
+    public String procesarLogin(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            Model model) {
+
+        if ("admin@gmail.com".equals(email) && "123".equals(password)) {
+            model.addAttribute("usuario", email);
+            return "redirect:/panelAdmin";
+        } else {
+            model.addAttribute("usuario", email);
+            return "principal";
+        }
+    }
+
+
 
     // Procesar registro (sin guardar aún)
     @PostMapping("/procesarRegistro")
